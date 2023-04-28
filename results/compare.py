d34005376b31a5cb796643b0c309ff7f0164fb1e
sofyc <857172970@qq.com>
2023-04-28 00:04:57 -0700

    upload

--- results/compare.py
+++ results/compare.py
@@ -1,6 +1,7 @@
 from metrics import psnr, ssim
 import torchvision.transforms as transforms
 import cv2
+import torch
 
 def png2tensor(image_path):
 	img = cv2.imread(image_path)
@@ -8,9 +9,17 @@ def png2tensor(image_path):
 	# img = cv2.merge([r, g, b])
 	transf = transforms.ToTensor()
 	img_tensor = transf(img)
+	img_tensor = img_tensor[:, 10:-10, 20:-20]
+	# print(img_tensor)
 	return img_tensor
 
-x, y = png2tensor("data/00004584_clear.png"), png2tensor("data/result.png")
+x, y = png2tensor("DCP/synthetic/00004584.png"), png2tensor("original/synthetic/00004584.png")
+meanx = torch.mean(x)
+meany = torch.mean(y)
+x = x * meany / meanx
+# print(meanx, meany)
+# exit()
+
 x = x.unsqueeze(0)
 y = y.unsqueeze(0)
 ssim1 = ssim(x, y).item()
